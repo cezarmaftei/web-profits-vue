2aa91fe5cb04fdaea80b79d4527c4a1ec05ee0c1
Maftei Cezar <cezar.maftei@gmail.com>
2022-02-01 16:39:29 +0200

    Removed GTM

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createGtm } from '@gtm-support/vue-gtm'
+// import { createGtm } from '@gtm-support/vue-gtm'
 
 import '@/assets/css/global.css'
 
 createApp(App)
   .use(router)
+  /*
   .use(
     createGtm({
       id: 'GTM-P2K3QJ',
       defer: 'defer'
     })
   )
+  */
   .mount('#webprofits-website')
