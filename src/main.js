9b7a316e0235666635afb9d9ee5fbde79d45e0b6
Maftei Cezar <cezar.maftei@gmail.com>
2022-02-01 16:32:28 +0200

    Added GTM

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createGtm } from '@gtm-support/vue-gtm'
 
 import '@/assets/css/global.css'
 
 createApp(App)
   .use(router)
+  .use(
+    createGtm({
+      id: 'GTM-P2K3QJ',
+      defer: 'defer'
+    })
+  )
   .mount('#webprofits-website')
