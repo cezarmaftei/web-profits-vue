3f679b4f47f95c29f35e84a04c6e03bbf1f46fb8
Maftei Cezar <cezar.maftei@gmail.com>
2022-02-01 11:01:41 +0200

    Unused css 4th try v3

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,12 @@ module.exports = {
               const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
               return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
             },
-            safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+            safelist: [
+              /-(leave|enter|appear)(|-(to|from|active))$/,
+              /^(?!(|.*?:)cursor-move).+-move$/,
+              /^router-link(|-exact)-active$/,
+              /data-v-.*/
+            ],
             fontFace: true
           })
         ]
